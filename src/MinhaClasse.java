e015377bd8d39e728b31a838a791d528a1718d80
Leandro Matos <leandro.matos@live.com>
2023-08-21 15:30:02 -0300

    Declarando variáveis e métodos - 2

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,8 +1,19 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        String meuNome = "Leandro";
+        /*String meuNome = "Leandro";
         int anoNascimento = 1990;
         booLean verdadeira = true;
-        anoNascimento = 1989;
+        anoNascimento = 1989;*/
+
+        String primeiroNome = "Leandro";
+        String segundoNome = "Matos";
+
+        String nomeCompleto = nomeCompleto (primeiroNome,segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome) {
+        return "Resultado do método " + primeiroNome.concat(" ").concat(segundoNome);
     }
 }
