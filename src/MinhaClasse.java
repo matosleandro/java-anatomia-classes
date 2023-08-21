4736b9cf04288495fedc95434f5467edf364bf5c
Leandro Matos <leandro.matos@live.com>
2023-08-21 15:20:16 -0300

    Declarando variáveis e métodos

--- src/MinhaClasse.java
+++ src/MinhaClasse.java
@@ -1,5 +1,8 @@
 public class MinhaClasse {
     public static void main(String[] args) {
-        System.out.print ("Olá turma, sejam bem vindos");
+        String meuNome = "Leandro";
+        int anoNascimento = 1990;
+        booLean verdadeira = true;
+        anoNascimento = 1989;
     }
 }
